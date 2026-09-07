34132eb5313a99fb7b8a60c9a4b8c16e7b46dd75
ShlomoYoseph <shlomo.developer@gmail.com>
2026-09-07 12:27:29 +0300

    Add one-click Windows installer build

---
+++ InterviewCopilot/build_windows.ps1
@@ -0,0 +1,9 @@
+$ErrorActionPreference = 'Stop'
+Set-Location $PSScriptRoot
+py -m venv .build-venv
+$python = Join-Path $PWD '.build-venv\Scripts\python.exe'
+& $python -m pip install --upgrade pip
+& $python -m pip install -r requirements.txt pyinstaller
+& $python -m playwright install chromium
+& $python -m PyInstaller --noconfirm --clean InterviewCopilot.spec
+Write-Host "Build complete: dist\InterviewCopilot"
